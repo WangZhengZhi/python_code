9f86dd42bfd404068a25d889620f687e455e1384
王正直 <wang_zheng_zhi@outlook.com>
2017-04-17 21:44:55 +0800

    这个是很久之前的代码，我忘了推送了，大概是2017年春节前后的

---
+++ helloworld.py
@@ -0,0 +1,58 @@
+message="hello python!"
+print(message)
+message="I am a hadsome boy"
+print(message)
+name='ada lovelace'
+print(name.title())#title()函数，已首字母大写的方式显示每一个单词
+print(name.lower())#lower()函数，小写的方式显示每一个单词
+print(name.upper())#upper()函数，大写的方式显示每一个单词
+first_name='ada'
+last_name='lovelace'
+full_name=first_name+" "+last_name
+print(full_name)
+message=first_name+" "+last_name+"!"
+print(message)
+message="hello"+" "+full_name.title()+"!"
+print(message)
+print("python")
+print("\tpython")#\t是换表符号
+print("\tLanguage:\nPython\nC\nJavaScript\n")
+#删除空白
+favorite_language='python '
+print(favorite_language)
+print(favorite_language.rstrip())#retrip()暂时性删除字符串末尾的空白，再次调用则会消失
+print(favorite_language)
+print(favorite_language.lstrip())#lstrip()函数删除字符串开头的空白
+print(favorite_language.strip())#strip()函数删除字符串两端的空白
+message="Tom's name is a people"
+print(message)
+print("i am a good boy")
+name="eric"
+words="Do u want to use python with me ?"
+message="hello"+" "+name.title()+" "+words
+print(message)
+name=name.title()
+name=name.upper()
+name=name.lower()
+print(name)
+#数字计算
+print(2+3)
+print(2/3)
+print(2**3)
+print(2*3)
+print(0.10+0.10)
+print(0.2+0.1)
+age=20
+message="hello "+str(age)+" "+"birthday! "
+print(message)
+
+
+
+
+
+
+
+
+
+
+
